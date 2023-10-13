7ca0d7d51c9a2b6c8143402fcd57719a58d35fe0
Axel Perez <axel-525@hotmail.com>
2023-10-13 13:16:36 -0600

    Se agregó la función para la selección de padres y se definió la función cruza de extremos

--- functions.py
+++ functions.py
@@ -1,38 +1,44 @@
 import random
 
-# INICIALIZACION}
+
+# INICIALIZACION
 
 # Generar una lista de 8 números aleatorios entre 0 y 7 (representando las columnas)
 def individuo():
     return random.sample(range(8), 8)
+
 
 # Inicio con 50 individuos aleatorios
 poblacion = []
 for i in range(50):
     poblacion.append(individuo())
 
+
 # FITNESS
 def fitness(tablero):
     ataques = 0
-    for i in range(8):
-        for j in range(i + 1, 8):
-            if tablero[i] == tablero[j] or abs(tablero[i] - tablero[j]) == abs(i - j):
-                ataques += 1
-    return 28 - ataques
+    ataques = sum(1 for i in range(8) for j in range(i + 1, 8) if
+                  tablero[i] == tablero[j] or abs(tablero[i] - tablero[j]) == abs(i - j))
+    return 1 / (ataques + 1)
 
 
-
+# ejem = [0, 1, 2, 3, 4, 5, 6, 7]
+#
+# print(fitness(ejem))
 
 
 # SELECCION DE PADRES UNIVERSAL ESTOCASTICA
 
 # Probabilidades acumuladas lista
+
+# miu es el numero de poblacion total
+# lamnda es el numero de padres que voy a escoher
+
 def proba_ac(pob):
     suma = 0
     acum = 0.0
     probabilidades = []
-    for i in pob:
-        suma = suma + fitness(i)
+    suma = sum(fitness(i) for i in pob)
     for i in pob:
         proba = fitness(i) / suma
         acum = acum + proba
@@ -40,18 +46,75 @@ def proba_ac(pob):
     return probabilidades
 
 
+# SELECCION DE PADRE UNIVERSAL ESTOCASTICA
+# Given the cumulative probability distribution 'ac'
+# and assuming we wish to select 'lam' members of the mating pool
+current_member = 1
+k = 1
+lam = 4
+r = random.uniform(0, 1 / lam)
+padres = []
+
+while current_member <= lam:
+    while r <= proba_ac(poblacion)[k]:
+        padres.append(poblacion[current_member])
+        r += 1 / lam
+        current_member += 1
+    k += 1
 
 
-# Seleccion de padres universal estocastica
+# en ves de guardar una losta de padres seleccionados, guardar una lista con los indices correspondientes en la poblacion
 
-# k = 1
-# lam = len(poblacion)
-# r = random.uniform(0, 1/lam )
-# while k <= lam:
-#     while r <= ac[k]:
-#         poblacion[k]
+# escojo 25 papas y 25 mamas, luego de cada pareja voy a tener dos hijos 'distinots', ya de ahi la poblacion es de 50 y los 50 hijos que genere
 
-# CRUZA
+
+# CRUZA A LOS EXTREMOS
+
+
+def crear_tabla(papa, mama):
+    tabla_extr = {}
+    for i in range(8):
+        tabla_extr[i] = [
+            papa[(i + 1) % 8],
+            papa[(i - 1) % 8],
+            mama[(i + 1) % 8],
+            mama[(i - 1) % 8]
+        ]
+    return tabla_extr
+
+
+def cruza_extr(papa, mama):
+    tabla = crear_tabla(papa, mama)
+    inicial = random.choice(papa)
+    hijo = [inicial]
+    current_element = inicial
+    current_nuevo = None
+
+    while len(hijo) < 8:
+        for element in tabla:
+            tabla[element] = [m for m in tabla[element] if m != current_element]
+            if not tabla[element]:
+                del tabla[element]
+
+        vecinos = tabla[current_element]
+        for numero in vecinos:
+            if vecinos.count(numero) > 1:
+                current_nuevo = numero
+                break
+            elif current_nuevo is None:
+                current_nuevo = min(tabla.values())
+            else:
+                current_nuevo = random.randint(0,7)
+        current_element = current_nuevo
+    return hijo
+
+
+tabla = cruza_extr([0,1,2,3,4,5,6,7], [3,7,0,2,6,5,1,4])
+
+print(tabla)
+
+
+
 
 
 # MUTACION
@@ -69,8 +132,4 @@ def mezcla(individuo):
 
     return individuo
 
-
-tabbb = [0, 1, 2, 3, 4, 5, 6, 7]
-tabbb_m = mezcla(tabbb)
-print(fitness(tabbb))
 
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 print("hola mundo")
 
 # inicializacion: aleatoria ---YA
-# sel de padres universal estocastica
+# sel de padres universal estocastica YA?
 # cruza a los extremos
 # mutacion mezcla ---YA
 # reemplazo: brecha generacional
+
